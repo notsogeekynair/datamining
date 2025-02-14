8f3f8341dc8b39f0f0831d56afc40a40fa66c80e
alina <130225239+alina15andreeva@users.noreply.github.com>
2025-02-13 19:13:51 -0500

    Update proprocessing_encode.R

--- proprocessing_encode.R
+++ proprocessing_encode.R
@@ -1,3 +1,150 @@
+df <- read.csv('project_data.csv')
+dim(df)
+head(df)
+sapply(df, class)
+
+df <- df[, !(names(df) %in% c("SERIALNO"))]
+
+# DATA CLEANING
+# Check for duplicate rows
+cat("Number of duplicate rows:", nrow(df[duplicated(df), ]))
+
+# Check the number of missing values in each column
+sapply(df, function(x) sum(is.na(x)))
+
+# Remove the columns with more than 80% of missing values
+drop_columns <- names(which(colMeans(is.na(df)) > 0.8))
+drop_columns
+df <- df[, !names(df) %in% drop_columns]
+dim(df)
+
+sapply(df, function(x) sum(is.na(x)))
+
+impute_map <- list(
+  COW = 0, POVPIP = -1, JWRIP = 0, ENG = 0, PERNP = -1,
+  JWMNP = 0, FER = 0, SCIENGRLP = 0, WKHP = 0, GCL = 0, SCIENGP = 0, MARHYP = -1,
+  RC = -1, WKWN = 0, POWSP = 0, YOEP = -1, MARHD = 0, POWPUMA = 0,
+  MARHM = 0, PAOC = 0, MARHT = 0, OC = -1, MARHW = 0, MIL = 0,
+  LANP = 0, JWDP = 0,
+  JWAP = 0, INDP = 0, FOD1P = 0,
+  ESR = 0, NWAB = 0, NWAV = 0, DRIVESP = 0, NWLA = 0, DECADE = 0,
+  NWLK = 0, OCCP = 0, NWRE = 0, WRK = 0, JWTRNS = 0
+)
+
+# Loop through each column and impute the specified value
+for (col in names(impute_map)) {
+  if (col %in% names(df)) {
+    df[[col]][is.na(df[[col]])] <- impute_map[[col]]
+  }
+}
+
+sapply(df, function(x) sum(is.na(x)))
+
+# DATA TYPES CORRECTION
+# Check the current data type of each variable
+sapply(df, class)
+drop_columns
+cols_to_factor <- c("RT", "DIVISION", "PUMA", "REGION",
+                   "STATE", "ADJINC", "CIT", "COW", "ENG",
+                   "FER", "GCL", "HIMRKS", 
+                   "HINS1", "HINS2", "HINS3", "HINS4",
+                   "HINS5", "HINS6", "HINS7", "JWTRNS", 
+                   "LANX", "MAR", "MARHD", "MARHM", "MARHT", 
+                   "MARHW", "MIG", "MIL", "NWAB", 
+                   "NWAV", "NWLA", "NWLK", "NWRE", "SCH",
+                   "SCHL", "SEX", "WRK", "ANC", "ANC1P", "ANC2P",
+                   "DECADE", "DRIVESP", "ESR", "FOD1P",
+                   "HICOV", "HISP", "INDP", "JWAP", "JWDP", "LANP",
+                   "MSP", "NATIVITY", "OC", "OCCP", "PAOC",
+                   "POBP", "POWPUMA", "POWSP", "PRIVCOV", "PUBCOV", "QTRBIR",
+                   "RAC1P", "RAC2P", "RAC3P", "RACAIAN", "RACASN", "RACBLK",
+                   "RACNH", "RACNUM", "RACPI", "RACSOR", "RACWHT", "RC", "SCIENGP",
+                   "SCIENGRLP", "WAOB", "Class")
+
+df[cols_to_factor] <- lapply(df[cols_to_factor], as.factor)
+
+# Convert integer variables in int_to_numeric to numeric
+int_to_numeric <- sapply(df, is.integer)
+df[int_to_numeric] <- lapply(df[int_to_numeric], as.numeric)
+
+
+sapply(df, class)
+
+
+
+# Check for near zero variance variables
+install.packages("caret")
+library(caret)
+low_var_col <- nearZeroVar(df, names = TRUE)
+low_var_col
+df <- df[, !(names(df) %in% low_var_col)]
+dim(df)
+
+
+# OUTLIERS HANDLING
+# Visualize numeric data with boxplots
+install.packages("reshape2")
+library(reshape2)
+library(ggplot2)
+numeric_columns <- sapply(df, is.numeric)
+df_long <- melt(df[, numeric_columns])
+
+ggplot(df_long, aes(x = variable, y = value)) +
+  geom_boxplot(outlier.size = 1) +
+  scale_y_continuous(trans = 'pseudo_log') +
+  theme_minimal() +
+  labs(x = "Variables", y = "Values", title = "Boxplots of Numeric Variables")
+
+
+
+for (col in names(df)[numeric_columns]) {
+  Q1 <- quantile(df[[col]], 0.25, na.rm = TRUE)
+  Q3 <- quantile(df[[col]], 0.75, na.rm = TRUE)
+  IQR <- Q3 - Q1
+  
+  LB <- Q1 - 1.5 * IQR
+  UB <- Q3 + 1.5 * IQR
+  
+  outliers <- df[[col]] < LB | df[[col]] > UB
+  
+  outlier_count <- sum(outliers, na.rm = TRUE)
+  cat(paste("Column:", col, "- Outliers:", outlier_count, "\n"))
+}
+
+outlier_cols_handle <- c("PWGTP", "WAGP", "PERNP", "PINCP")
+
+# Handle the outliers based on IQR (assign them to lower or upper bounds)
+for (col in outlier_cols_handle) {
+  Q1 <- quantile(df[[col]], 0.25, na.rm = TRUE)
+  Q3 <- quantile(df[[col]], 0.75, na.rm = TRUE)
+  IQR <- Q3 - Q1
+  
+  LB <- Q1 - 1.5 * IQR
+  UB <- Q3 + 1.5 * IQR
+  
+  df[[col]][df[[col]] < LB] <- LB
+  df[[col]][df[[col]] > UB] <- UB
+}
+
+df_long <- melt(df[, numeric_columns])
+
+ggplot(df_long, aes(x = variable, y = value)) +
+  geom_boxplot(outlier.size = 1) +
+  scale_y_continuous(trans = 'pseudo_log') +
+  theme_minimal() +
+  labs(x = "Variables", y = "Values", title = "Boxplots of Numeric Variables")
+
+sapply(df, class)
+
+
+h <- hist(df$PWGTP, breaks=5, plot=FALSE)
+h$breaks
+
+df$PWGTP <- as.numeric(cut(df$PWGTP, breaks=4, labels=c(0, 1, 2, 3)))
+df$PWGTP
+hist(df$PWGTP)
+
+library(dplyr)
 df <- df %>%
   mutate(
     MARHYP = case_when(
@@ -10,3 +157,6 @@ df <- df %>%
       TRUE ~ NA_real_                  # Handle unexpected values
     )
   )
+
+table(df$NWLK[is.na(df$NWAB)])
+unique(df$OCCP)
